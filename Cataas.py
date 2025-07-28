21c945efd994b8c6791b308eac22c9041d7a869b
OlyaShmagel <olgashmagel1@gmail.com>
2025-07-28 14:32:49 +0300

    Теперь каждое изображение открывается в новом окне

--- Cataas.py
+++ Cataas.py
@@ -17,11 +17,16 @@ def load_image(url):
         return None
 
 
-def set_image():
+def open_new_window():
     img = load_image(url)
+
     if img:
-        label.config(image=img)
-        label.image = img
+        new_window = Toplevel()
+        new_window.title('Картинка с котиком')
+        new_window.geometry('600x480')
+        label = Label(new_window, image=img)
+        label.pack()
+
 
 
 def exit():
@@ -31,8 +36,7 @@ window = Tk()
 window.title('Cats')
 window.geometry('600x520')
 
-label = Label()
-label.pack()
+
 
 # update_button = Button(text='Обновить', command=set_image)
 # update_button.pack()
@@ -41,10 +45,10 @@ menu_bar = Menu(window)
 window.config(menu=menu_bar)
 
 file_menu = Menu(menu_bar, tearoff=0)
-menu_bar.add_cascade(Label='Файл', menu=file_menu)
-file_menu.add_command(Label='Загрузить фото', command=set_image)
+menu_bar.add_cascade(label='Файл', menu=file_menu)
+file_menu.add_command(label='Загрузить фото', command=open_new_window)
 file_menu.add_separator()
-file_menu.add_command(Label='Выход', command=exit)
+file_menu.add_command(label='Выход', command=exit)
 
 url = 'https://cataas.com/cat'
 
